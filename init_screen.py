3a02ed6e9b501d892b3e790c6903f9bbb09aeb32
Beatriz Telles <beatriztf1@al.insper.edu.br>
2025-05-14 14:47:05 -0300

    Criando tela inicial do jogo

---
+++ init_screen.py
@@ -0,0 +1,41 @@
+import pygame
+import random 
+from os import path
+
+def init_screen(): 
+    # Variável para ajuste de velocidade 
+    clock = pygame.time.Clock()
+
+    # Carrega o fundo da tela inicial 
+    background = pygame.image.load(path.join(IMG_DIR, 'init_screen.png')).convert()
+    background_rect = background.get_rect()
+
+    running = True 
+    while running: 
+
+        # Ajusta a velocidade do jogo. 
+        clock.tick(FPS)
+
+        # Processa os enventos
+        for event in pygame.event.get():
+            # Verifica se foi fechado 
+            if event.type == pygame.QUIT:
+                state = QUIT
+                running = False 
+            
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_RETURN:
+                    state = GAME
+                    running = False 
+                if event.key == pygame.K_SPACE: 
+                    state = INFO
+                    running = False 
+        
+        # A cada loop, redesenha o fundo e os sprites
+        screen.fill(BLACK)
+        screen.blit(background, background_rect)
+
+        # Depois de desenhar tudo, inverte o display
+        pygame.display.flip()
+
+    return state
