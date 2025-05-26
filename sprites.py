98c23d00058d830862dc73991ea42eba9658fce7
GustavoGazel <gustavogs3@al.insper.edu.br>
2025-05-26 17:51:16 -0300

    Minion pulando + bananas movendo

--- sprites.py
+++ sprites.py
@@ -14,18 +14,18 @@ class Minion(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.centerx = -WIDTH
         self.rect.bottom = HEIGHT
-        self.speedx=0
         self.speedy = 0 
         self.groups = groups
         self.assets = assets
-
-
 
         # Mantem dentro da tela
         if self.rect.right > WIDTH:
             self.rect.right = WIDTH
         if self.rect.left < 0:
             self.rect.left = 0
+
+    def update(self):
+        self.rect.y += self.speedy
 
     
     def purple(self):
@@ -68,6 +68,10 @@ class Banana(pygame.sprite.Sprite):
         # Se o tiro passar do inicio da tela, morre.
         if self.rect.bottom < 0:
             self.kill()
+
+    def update(self):
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
 
 class Soro(pygame.sprite.Sprite):
     def __init__(self, assets, bottom, centerx):
